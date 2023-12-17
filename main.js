b85ba512e36b8104fbc0224b6a99f446c86b0a0d
bybert <74455519+bybert@users.noreply.github.com>
2023-12-17 21:32:14 +0300

    added send and show your message feature

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import { Telegraf, Markup} from 'telegraf'
+import { Telegraf, Markup } from 'telegraf'
+import { message }from 'telegraf/filters'
 
 const token = '6468309568:AAGZHoTM-eT_OvnCVA-BVYgBGQ7WZA0L4Qo'
 const webAppUrl = 'https://angular-tg-app-b6b75.web.app/'
@@ -6,10 +7,16 @@ const webAppUrl = 'https://angular-tg-app-b6b75.web.app/'
 const bot = new Telegraf(token)
 
 bot.command('start', (ctx) => {
-    ctx.reply(
-        'Добро пожаловать! Нажмите на кнопку ниже, чтобы запустить приложение',
-        Markup.keyboard([Markup.button.webApp('Отправить сообщение', webAppUrl)])
-    )
+  ctx.reply(
+    'Добро пожаловать! Нажмите на кнопку ниже, чтобы запустить приложение',
+    Markup.keyboard([
+      Markup.button.webApp('Отправить сообщение', `${webAppUrl}/feedback`),
+    ])
+  )
 })
 
+bot.on(message('web_app_data'), async ctx => {
+    const data = ctx.webAppData.data.json()
+    ctx.reply(`Ваше сообщение: ${data?.feedback}` ?? 'empty message')
+})
 bot.launch()
